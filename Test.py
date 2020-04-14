3a4d7b76f09754d852ccffc3290dadf5c2aa4a11
LengGeng <46020226+LengGeng@users.noreply.github.com>
2020-04-14 15:37:30 +0800

    上传文件
    练习上传文件

---
+++ Test.py
@@ -0,0 +1,10 @@
+# _*_ coding  :  UTF-8 _*_
+# 开发团队  :   冷羹
+# 开发人员  :   冷羹
+# 开发时间  :   2019/4/7 23:28
+# 文件名称  :   Test.PY
+# 开发工具  :   PyCharm
+
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
